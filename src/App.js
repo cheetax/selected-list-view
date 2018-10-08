08b02fa19c0cb802c14f434bcf86f66df98ed8f8
cheetax <dmitriy.grebenev@gmail.com>
2018-10-08 17:09:59 +0300

    new

--- src/App.js
+++ src/App.js
@@ -12,17 +12,34 @@ class App extends Component {
     }
   }
 
+  rowRenderer = ({
+    key,         // Unique key within array of rows
+    index,       // Index of row within collection
+    isScrolling, // The List is currently being scrolled
+    isVisible,   // This row is visible within the List (eg it is not an overscanned row)
+    style        // Style object to be applied to row (to position it)
+  }) => {
+    if (this.props.users.length - 1 < index) return null;
+    return [
+      <span style={{ paddingRight: 10, }} >{this.props.users[index].firstName} {this.props.users[index].surName}</span>,
+      <span style={{ paddingRight: 10 }} >{this.props.users[index].email}</span>
+    ]
+  }
+
+  headerRenderer = (param) => [<span width={150} style={param.style}   >Имя, Фамилия</span>,
+  <span style={param.style}  >Email</span>]
+
   render() {
-//    console.log(this.state)
+    //    console.log(this.state)
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Test Input Field</h1>
-        </header> 
+        </header>
         <h3>Filled text fields</h3>
         <div className='App-intro' >
-          <SelectedListView isField isButtonActive dateFrom={this.state.dateFrom} dateTo={this.state.dateTo} onSelect={(period) => this.setState({...period})} />          
-        </div>        
+          <SelectedListView isField isButtonActive dateFrom={this.state.dateFrom} dateTo={this.state.dateTo} onSelect={(period) => this.setState({ ...period })} />
+        </div>
       </div>
     );
   }
