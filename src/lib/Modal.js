ea44172091548706aebdbc1ed0ea07c520527565
cheetax <dmitriy.grebenev@gmail.com>
2018-11-09 16:26:55 +0300

    111

--- src/lib/Modal.js
+++ src/lib/Modal.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import ListViewCore from './list-view-core';
 import './Modal.css'
-const ClassModal = ({ openModalFlex, openModalExpand, openFlex }) => (((openFlex) ? 'modal-dialog-flex ' + ((openModalFlex) ? 'active': '') : 'modal-dialog-button ' + ((openModalExpand) ? 'active': '')) )
+const ClassModal = ({ openModalFlex, openModalExpand, openFlex }) => (((openFlex) ? 'modal-dialog-flex ' + ((openModalFlex) ? 'active' : '') : 'modal-dialog-button ' + ((openModalExpand) ? 'active' : '')))
 
-const ClassModalOverlay = ({ openModal, openFlex }) => ((openFlex) ? ('modal-dialog-overlay ' + ((openModal) ? 'active' : '')) : ((!openModal) ?  'modal-dialog-button-overlay' : ''))
+const ClassModalOverlay = ({ openModal, openFlex }) => ((openFlex) ? ('modal-dialog-overlay ' + ((openModal) ? 'active' : '')) : ((!openModal) ? 'modal-dialog-button-overlay' : ''))
 
 const positiveNum = (num) => num < 0 ? 0 : num
 
@@ -22,9 +22,9 @@ export class Modal extends Component {
         this.setState({ elem })
     }
 
-    componentDidUpdate(prevProps, prevState, snap) {
-        if (this.state.elem && !this.props.openFlex) {
-            let props = this.props
+    componentWillReceiveProps(nextProps) {
+        if (this.state.elem && !nextProps.openFlex) {
+            let props = nextProps;
             let left = (props.elemSize) && positiveNum(props.elemSize.offsetLeft - this.state.elem.clientWidth) + 'px' || 0
             let clientWidth = props.Width && props.Width || this.state.clientWidth;
             let clientHeight = props.Height && props.Height || this.state.clientHeight;
@@ -35,6 +35,7 @@ export class Modal extends Component {
             let top = (props.elemSize) && ((props.elemSize.offsetTop < (props.elemSize.innerHeight - this.state.elem.clientHeight)) ? props.elemSize.offsetTop : positiveNum(props.elemSize.innerHeight - this.state.elem.clientHeight)) + 'px' || 0;
             (this.state.left !== left || this.state.top !== top) && this.setState({ left, top, clientHeight, clientWidth })
         }
+
     }
 
     _style = () => {
@@ -67,7 +68,7 @@ export class Modal extends Component {
         >
             {this.props.elemSize && <div ref={this._ref} style={this._style()} className={'modal-flex-column ' + ClassModal({ openModalFlex, openModalExpand, openFlex })} >
                 {this.props.openModal && <ListViewCore {...this.props} />}
-                {this.props.children && <div style={{margin: '8px 8px 0 8px'}} >{this.props.children}</div> }
+                {this.props.children && <div style={{ margin: '8px 8px 0 8px' }} >{this.props.children}</div>}
             </div>}
 
         </div>
