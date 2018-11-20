2ecf714b84a6ef43fbd89e60dda09cb93c8245fa
cheetax <dmitriy.grebenev@gmail.com>
2018-11-20 16:53:21 +0300

    111

--- src/lib/BtnSpin.js
+++ src/lib/BtnSpin.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './BtnSpin.css'
 
-const BtnSpin = ({ onClick, children }) => <div className='btn-spin browser-default'
+const BtnSpin = ({ onClick, children, className = '', size = 32 }) => <div className={(() => 'btn-spin browser-default ' + className)()}
+    style={{ height: size, width: size }}
     onClick={onClick ? (event) => onClick(event) : null}>
     <input
         type='url'
         readOnly
+        style={{ height: size, width: size }}
         //value=''
-        className='btn-spin browser-default'>
+        className={(() => 'btn-spin browser-default ' + className)()}>
     </input>{children}</div>
 
 export {
